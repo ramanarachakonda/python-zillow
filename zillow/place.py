e10b858b1749025b3c2736dc48670c005bcd516d
ramanarachakonda <ramanarachakonda@users.noreply.github.com>
2017-07-04 18:24:59 -0500

    Update place.py
    Fixed zrentestimate issues

--- zillow/place.py
+++ zillow/place.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from abc import abstractmethod
 
 class SourceData(classmethod):
@@ -12,7 +14,7 @@ class SourceData(classmethod):
     @abstractmethod
     def debug(self):
         for i in self.__dict__.keys():
-            print "%s: %s" % (i, self.__dict__[i])
+            print("%s: %s" % (i, self.__dict__[i]))
 
     @abstractmethod
     def get_dict(self):
@@ -43,13 +45,11 @@ class Links(SourceData):
         :source_data: Data from data.get('SearchResults:searchresults', None)['response']['results']['result']['links']
         :return:
         """
-        self.home_details = source_data['homedetails']
-        try:
-            self.graphs_and_data = source_data['graphsanddata']
-        except:
-            self.graphs_and_data = None
-        self.map_this_home = source_data['mapthishome']
-        self.comparables = source_data['comparables']
+        if source_data:
+            self.home_details = source_data['homedetails'] if 'homedetails' in source_data else None
+            self.graphs_and_data = source_data['graphsanddata'] if 'graphsanddata' in source_data else None
+            self.map_this_home = source_data['mapthishome'] if 'mapthishome' in source_data else None
+            self.comparables = source_data['comparables'] if 'comparables' in source_data else None
 
 class FullAddress(SourceData):
     def __init__(self, **kwargs):
@@ -65,12 +65,12 @@ class FullAddress(SourceData):
         :source_data: Data from data.get('SearchResults:searchresults', None)['response']['results']['result']['address']
         :return:
         """
-        self.street = source_data['street']
-        self.zipcode = source_data['zipcode']
-        self.city = source_data['city']
-        self.state = source_data['state']
-        self.latitude = source_data['latitude']
-        self.longitude = source_data['longitude']
+        if source_data:
+            self.street = source_data['street'] if 'street' in source_data else None
+            self.zipcode = source_data['zipcode'] if 'zipcode' in source_data else None
+            self.state = source_data['state'] if 'state' in source_data else None
+            self.latitude = source_data['latitude'] if 'latitude' in source_data else None
+            self.longitude = source_data['longitude'] if 'longitude' in source_data else None
 
 class ZEstimateData(SourceData):
     def __init__(self, **kwargs):
@@ -86,24 +86,59 @@ class ZEstimateData(SourceData):
         :source_data: Data from data.get('SearchResults:searchresults', None)['response']['results']['result']['zestimate']
         :return:
         """
-        try:
-            self.amount = int(source_data['amount']['#text'])
-        except:
-            self.amount = None
-        self.amount_currency = source_data['amount']['@currency']
-        self.amount_last_updated = source_data['last-updated']
-        try:
-            self.amount_change_30days = int(source_data['valueChange']['#text'])
-        except:
-            self.amount_change_30days = None
-        try:
-            self.valuation_range_low = int(source_data['valuationRange']['low']['#text'])
-        except:
-            self.valuation_range_low = None
-        try:
-            self.valuation_range_high = int(source_data['valuationRange']['high']['#text'])
-        except:
-            self.valuation_range_high = None
+        if source_data:
+            try:
+                self.amount = int(source_data['amount']['#text'])
+            except:
+                self.amount = None
+            self.amount_currency = source_data['amount']['@currency'] if 'amount' in source_data and '@currency' in source_data['amount'] else None
+            self.amount_last_updated = source_data['last-updated'] if 'last-updated' in source_data else None
+            try:
+                self.amount_change_30days = int(source_data['valueChange']['#text'])
+            except:
+                self.amount_change_30days = None
+            try:
+                self.valuation_range_low = int(source_data['valuationRange']['low']['#text'])
+            except:
+                self.valuation_range_low = None
+            try:
+                self.valuation_range_high = int(source_data['valuationRange']['high']['#text'])
+            except:
+                self.valuation_range_high = None
+
+class RentZEstimateData(SourceData):
+    def __init__(self, **kwargs):
+        self.amount = None
+        self.amount_currency = None
+        self.amount_last_updated = None
+        self.amount_change_30days = None
+        self.valuation_range_low = None
+        self.valuation_range_high = None
+
+    def set_data(self, source_data):
+        """
+        :source_data: Data from data.get('SearchResults:searchresults', None)['response']['results']['result']['zestimate']
+        :return:
+        """
+        if source_data:
+            try:
+                self.amount = int(source_data['amount']['#text'])
+            except:
+                self.amount = None
+            self.amount_currency = source_data['amount']['@currency'] if 'amount' in source_data and '@currency' in source_data['amount'] else None
+            self.amount_last_updated = source_data['last-updated'] if 'last-updated' in source_data else None
+            try:
+                self.amount_change_30days = int(source_data['valueChange']['#text'])
+            except:
+                self.amount_change_30days = None
+            try:
+                self.valuation_range_low = int(source_data['valuationRange']['low']['#text'])
+            except:
+                self.valuation_range_low = None
+            try:
+                self.valuation_range_high = int(source_data['valuationRange']['high']['#text'])
+            except:
+                self.valuation_range_high = None
 
 class LocalRealEstate(SourceData):
     def __init__(self):
@@ -120,13 +155,14 @@ class LocalRealEstate(SourceData):
         :source_data": Data from data.get('SearchResults:searchresults', None)['response']['results']['result']['localRealEstate']
         :return:
         """
-        self.region_name = source_data['region']['@name']
-        self.region_id = source_data['region']['@id']
-        self.region_type =  source_data['region']['@type']
-        self.zillow_home_value_index = source_data.get('zindexValue', None)
-        self.overview_link =  source_data['region']['links']['overview']
-        self.fsbo_link =  source_data['region']['links']['forSaleByOwner']
-        self.sale_link =  source_data['region']['links']['forSale']
+        if source_data:
+            self.region_name    = source_data['region']['@name']   if 'region' in source_data and '@name' in source_data['region'] else None
+            self.region_id      = source_data['region']['@id']     if 'region' in source_data and '@id' in source_data['region'] else None
+            self.region_type    =  source_data['region']['@type']  if 'region' in source_data and '@type' in source_data['region'] else None
+            self.zillow_home_value_index = source_data.get('zindexValue', None)
+            self.overview_link  =  source_data['region']['links']['overview']  if 'region' in source_data and 'links' in source_data['region'] and 'overview' in source_data['region']['links'] else None
+            self.fsbo_link      =  source_data['region']['links']['forSaleByOwner'] if 'region' in source_data and 'links' in source_data['region'] and 'forSaleByOwner' in source_data['region']['links'] else None
+            self.sale_link      =  source_data['region']['links']['forSale'] if 'region' in source_data and 'links' in source_data['region'] and 'forSale' in source_data['region']['links'] else None
 
 class ExtendedData(SourceData):
     def __init__(self):
@@ -144,35 +180,38 @@ class ExtendedData(SourceData):
         self.complete = False
 
     def set_data(self, source_data):
-        self.fips_county = source_data.get('FIPScounty', None)
-        self.usecode = source_data['useCode']
-        self.tax_assessment_year = source_data.get('taxAssessmentYear', None)
-        self.tax_assessment = source_data.get('taxAssessment', None)
-        self.year_built = source_data.get('yearBuilt', None)
-        self.lot_size_sqft = source_data.get('lotSizeSqFt', None)
-        self.finished_sqft = source_data.get('finishedSqFt', None)
-        self.bathrooms = source_data.get('bathrooms', None)
-        self.bedrooms = source_data.get('bedrooms', None)
-        self.last_sold_date = source_data.get('lastSoldDate', None)
-        price_element = source_data.get('lastSoldPrice', None)
-        if price_element is not None:
-            self.last_sold_price = price_element.get('#text', None)
-        self.complete = True
+        if source_data:
+            self.fips_county = source_data.get('FIPScounty', None)
+            self.usecode = source_data['useCode']
+            self.tax_assessment_year = source_data.get('taxAssessmentYear', None)
+            self.tax_assessment = source_data.get('taxAssessment', None)
+            self.year_built = source_data.get('yearBuilt', None)
+            self.lot_size_sqft = source_data.get('lotSizeSqFt', None)
+            self.finished_sqft = source_data.get('finishedSqFt', None)
+            self.bathrooms = source_data.get('bathrooms', None)
+            self.bedrooms = source_data.get('bedrooms', None)
+            self.last_sold_date = source_data.get('lastSoldDate', None)
+            price_element = source_data.get('lastSoldPrice', None)
+            if price_element is not None:
+                self.last_sold_price = price_element.get('#text', None)
+            self.complete = True
 
 
 class Place(SourceData):
     """
     A class representing a property and it's details
     """
-    def __init__(self, has_extended_data=False):
+    def __init__(self, has_extended_data=False, has_rentzestimate_data=False):
         self.zpid = None
         self.links = Links()
         self.full_address = FullAddress()
-        self.zestiamte = ZEstimateData()
+        self.zestimate = ZEstimateData()        
+        self.rentzestimate = RentZEstimateData()
         self.local_realestate = LocalRealEstate()
         self.similarity_score = None
         self.extended_data = ExtendedData()
         self.has_extended_data = has_extended_data
+        self.has_rentzestimate_data = has_rentzestimate_data
 
     def set_data(self, source_data):
         """
@@ -180,12 +219,15 @@ class Place(SourceData):
         :param source_data:
         :return:
         """
-
+        if type(source_data).__name__ == 'list':
+            source_data = source_data[0]
         self.zpid = source_data.get('zpid', None)
         self.similarity_score = source_data.get('@score', None)
         self.links.set_data(source_data['links'])
         self.full_address.set_data(source_data['address'])
-        self.zestiamte.set_data(source_data['zestimate'])
+        self.zestimate.set_data(source_data['zestimate'])
+        if self.has_rentzestimate_data and 'rentzestimate' in source_data:
+            self.rentzestimate.set_data(source_data['rentzestimate'])
         self.local_realestate.set_data(source_data['localRealEstate'])
         if self.has_extended_data:
             self.extended_data.set_data(source_data)
@@ -196,12 +238,9 @@ class Place(SourceData):
             'similarity_score': self.similarity_score,
             'links': self.links.get_dict(),
             'full_address': self.full_address.get_dict(),
-            'zestimate': self.zestiamte.get_dict(),
+            'zestimate': self.zestimate.get_dict(),
+            'rentzestimate': self.rentzestimate.get_dict(),
             'local_realestate': self.local_realestate.get_dict(),
             'extended_data': self.extended_data.get_dict()
         }
         return data
-
-
-
-
